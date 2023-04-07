410e8fa2d2e7785e4ceb75497828b1a03c59da2c
Le Trung <trungle30902@gmail.com>
2023-04-07 09:20:48 +0700

    migrate: replace path & add i18n

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,25 +1,31 @@
 import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from 'node:url'
+import { resolve, dirname } from 'node:path'
 import vue from '@vitejs/plugin-vue'
 import eslint from 'vite-plugin-eslint'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import stylelint from 'vite-plugin-stylelint'
-import path from 'path'
+import vueI18nPlugin from '@intlify/vite-plugin-vue-i18n'
 
 export default defineConfig({
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@comp': path.resolve(__dirname, './src/components'),
-      '@layout': path.resolve(__dirname, './src/layouts'),
-      '@view': path.resolve(__dirname, './src/views'),
-      '@asset': path.resolve(__dirname, './src/assets'),
-      '@style': path.resolve(__dirname, './src/styles')
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@comp': fileURLToPath(new URL('./src/components', import.meta.url)),
+      '@layout': fileURLToPath(new URL('./src/layouts', import.meta.url)),
+      '@view': fileURLToPath(new URL('./src/views', import.meta.url)),
+      '@asset': fileURLToPath(new URL('./src/assets', import.meta.url)),
+      '@style': fileURLToPath(new URL('./src/styles', import.meta.url)),
+      '@type': fileURLToPath(new URL('./src/types', import.meta.url))
     }
   },
   plugins: [
     vue(),
     eslint(),
     tsconfigPaths(),
-    stylelint()
+    stylelint(),
+    vueI18nPlugin({
+      include: resolve(dirname(fileURLToPath(import.meta.url)), './src/i18n/locales/**')
+    })
   ]
 })
