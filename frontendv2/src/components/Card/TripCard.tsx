97ad5d07218aa504e4b106b90068d79221f61d70
Kiritoro <trungle30902@gmail.com>
2023-12-04 00:20:46 +0700

    Fix trip status & homestay request params

--- frontendv2/src/components/Card/TripCard.tsx
+++ frontendv2/src/components/Card/TripCard.tsx
@@ -1,4 +1,11 @@
-import React, { useCallback, useLayoutEffect, useMemo, useState, useEffect, ChangeEvent } from "react";
+import React, {
+  useCallback,
+  useLayoutEffect,
+  useMemo,
+  useState,
+  useEffect,
+  ChangeEvent,
+} from "react";
 import * as S from "@style/comp/Card/TripCard.styled";
 import { IBookingCreateResponse } from "@store/booking/booking.schema";
 import * as formatUtils from "@util/FormatUtils";
@@ -6,21 +13,31 @@ import { useRecoilState, useRecoilValue } from "recoil";
 import { authSelector } from "@store/user/user.selectors";
 import { useBookingActions } from "@store/booking/booking.actions";
 import { useNavigate } from "react-router-dom";
-import { isShowCancelPolicyModalAtom, isShowTripDetailModalAtom, selectedBookingIdAtom } from "@store/app.atoms";
+import {
+  isShowCancelPolicyModalAtom,
+  isShowTripDetailModalAtom,
+  selectedBookingIdAtom,
+} from "@store/app.atoms";
 import { CancelPolicyModal } from "@comp/Modal/CancelPolicyModal";
 import { TripDetailModal } from "@comp/Modal/TripDetailModal";
 
 interface Props {
-  booking: IBookingCreateResponse
+  booking: IBookingCreateResponse;
 }
 
 export const Tripcard = (props: Props) => {
   const { booking } = props;
   const { user } = useRecoilValue(authSelector);
-  const [isShowCancelPolicyModal, setIsShowCancelPolicyModal] = useRecoilState(isShowCancelPolicyModalAtom);
-  const [isShowTripDetailModal, setIsShowTripDetailModal] = useRecoilState(isShowTripDetailModalAtom);
-  const [selectedBookingId, setSelectedBokkingId] = useRecoilState(selectedBookingIdAtom);
-
+  const [isShowCancelPolicyModal, setIsShowCancelPolicyModal] = useRecoilState(
+    isShowCancelPolicyModalAtom
+  );
+  const [isShowTripDetailModal, setIsShowTripDetailModal] = useRecoilState(
+    isShowTripDetailModalAtom
+  );
+  const [selectedBookingId, setSelectedBokkingId] = useRecoilState(
+    selectedBookingIdAtom
+  );
+  console.log(booking);
   const bookingActions = useBookingActions();
   const navigate = useNavigate();
 
@@ -28,74 +45,126 @@ export const Tripcard = (props: Props) => {
     const username = user?.username!;
     const homestayId = booking.homestay_booking.id.toString();
     const bookingId = booking.booking.booking_id;
-    bookingActions.abort(username, homestayId, bookingId)
-      .then((res) => {})
-  }, [booking])
+    bookingActions.abort(username, homestayId, bookingId).then((res) => {});
+  }, [booking]);
 
   const handleViewDetailMouseClick = useCallback(() => {
     setSelectedBokkingId(booking.booking.booking_id);
     setIsShowTripDetailModal(true);
-  }, [])
+  }, []);
 
   const handleCancleButtonMouseClick = useCallback(() => {
     setSelectedBokkingId(booking.booking.booking_id);
     setIsShowCancelPolicyModal(true);
-  }, [])
+  }, []);
 
   const handleReBooking = useCallback(() => {
     navigate(`/homestay/${booking.homestay_booking.id}`);
-  }, [booking])
+  }, [booking]);
 
   useEffect(() => {
     setIsShowCancelPolicyModal(false);
     setSelectedBokkingId(null);
-  }, [])
+  }, []);
 
   return (
     <S.StyledContainer>
-      <S.StyledImage onClick={ handleViewDetailMouseClick } src={ booking.homestay_booking.main_image }/>
-      <S.StyledBooInfoWrapper onClick={ handleViewDetailMouseClick } >
-        <S.StyledBookInfoTitle onClick={ handleViewDetailMouseClick }>Mã đặt phòng/đặt chỗ: &nbsp; </S.StyledBookInfoTitle>
-        <S.StyledBookInfoValue>{ booking.booking.booking_id }</S.StyledBookInfoValue>
+      <S.StyledImage
+        onClick={handleViewDetailMouseClick}
+        src={booking.homestay_booking.main_image}
+      />
+      <S.StyledBooInfoWrapper onClick={handleViewDetailMouseClick}>
+        <S.StyledBookInfoTitle onClick={handleViewDetailMouseClick}>
+          Mã đặt phòng/đặt chỗ: &nbsp;{" "}
+        </S.StyledBookInfoTitle>
+        <S.StyledBookInfoValue>
+          {booking.booking.booking_id}
+        </S.StyledBookInfoValue>
       </S.StyledBooInfoWrapper>
-      <S.StyledBooInfoWrapper onClick={ handleViewDetailMouseClick }>
-        <S.StyledBookInfoTitle>Trạng thái thanh toán: &nbsp; </S.StyledBookInfoTitle>
-        { booking.payment.status == "unpaid"
-          ? <S.StyledBookInfoValue style={{ color: "#7D97B8" }}> Chưa thanh toán </S.StyledBookInfoValue>
-          : booking.payment.status == "invalidated"
-          ? <S.StyledBookInfoValue style={{ color: "#C13515" }}> Hủy thanh toán </S.StyledBookInfoValue>
-          : <S.StyledBookInfoValue style={{ color: "#3DC373" }}> Đã thanh toán </S.StyledBookInfoValue>
-        }
+      <S.StyledBooInfoWrapper onClick={handleViewDetailMouseClick}>
+        <S.StyledBookInfoTitle>
+          Trạng thái thanh toán: &nbsp;{" "}
+        </S.StyledBookInfoTitle>
+        {booking.payment.status == "unpaid" ? (
+          <S.StyledBookInfoValue style={{ color: "#7D97B8" }}>
+            {" "}
+            Chưa thanh toán{" "}
+          </S.StyledBookInfoValue>
+        ) : booking.payment.status == "invalidated" ? (
+          <S.StyledBookInfoValue style={{ color: "#C13515" }}>
+            {" "}
+            Hủy thanh toán{" "}
+          </S.StyledBookInfoValue>
+        ) : (
+          <S.StyledBookInfoValue style={{ color: "#3DC373" }}>
+            {" "}
+            Đã thanh toán{" "}
+          </S.StyledBookInfoValue>
+        )}
       </S.StyledBooInfoWrapper>
-      <S.StyledBooInfoWrapper onClick={ handleViewDetailMouseClick } >
-        <S.StyledBookInfoTitle>Trạng thái chuyến đi: &nbsp; </S.StyledBookInfoTitle>
-        { booking.booking.status == "validated"
-          ? <S.StyledBookInfoValue style={{ color: "#7D97B8" }}>Chưa nhận phòng </S.StyledBookInfoValue>
-          : booking.booking.status == "cancel"
-          ? <S.StyledBookInfoValue style={{ color: "#C13515" }}> Đã hủy bỏ </S.StyledBookInfoValue>
-          : <S.StyledBookInfoValue style={{ color: "#3DC373" }}> Đã nhận phòng </S.StyledBookInfoValue>
-        }
+      <S.StyledBooInfoWrapper onClick={handleViewDetailMouseClick}>
+        <S.StyledBookInfoTitle>
+          Trạng thái chuyến đi: &nbsp;{" "}
+        </S.StyledBookInfoTitle>
+        {booking.booking.status == "validated" ? (
+          <S.StyledBookInfoValue style={{ color: "#7D97B8" }}>
+            Đang chờ xác nhận{" "}
+          </S.StyledBookInfoValue>
+        ) : booking.booking.status == "cancel" ? (
+          <S.StyledBookInfoValue style={{ color: "#C13515" }}>
+            {" "}
+            Đã hủy bỏ{" "}
+          </S.StyledBookInfoValue>
+        ) : (
+          <S.StyledBookInfoValue style={{ color: "#05D619" }}>
+            {" "}
+            Đã xác nhận{" "}
+          </S.StyledBookInfoValue>
+        )}
       </S.StyledBooInfoWrapper>
-      <S.StyledLineHoz/>
+      <S.StyledLineHoz />
 
-      <S.StyledDateInfoWrapper onClick={ handleViewDetailMouseClick } >
+      <S.StyledDateInfoWrapper onClick={handleViewDetailMouseClick}>
         <S.StyledDateCheckInWrapper>
           <S.StyledDateInfoTitle>Ngày nhận phòng</S.StyledDateInfoTitle>
-          <S.StyledDateInfoValue>{booking.booking.status != "cancel" ? formatUtils.formatDateString(new Date(booking.booking.checkin_date)) : "___" }</S.StyledDateInfoValue>
+          <S.StyledDateInfoValue>
+            {booking.booking.status != "cancel"
+              ? formatUtils.formatDateString(
+                  new Date(booking.booking.checkin_date)
+                )
+              : "___"}
+          </S.StyledDateInfoValue>
         </S.StyledDateCheckInWrapper>
-        <S.StyledDateInfoLineVez/>
+        <S.StyledDateInfoLineVez />
         <S.StyledDateCheckOutWrapper>
           <S.StyledDateInfoTitle>Ngày trả phòng</S.StyledDateInfoTitle>
-          <S.StyledDateInfoValue>{booking.booking.status != "cancel" ? formatUtils.formatDateString(new Date(booking.booking.checkout_date)) : "___" }</S.StyledDateInfoValue>
+          <S.StyledDateInfoValue>
+            {booking.booking.status != "cancel"
+              ? formatUtils.formatDateString(
+                  new Date(booking.booking.checkout_date)
+                )
+              : "___"}
+          </S.StyledDateInfoValue>
         </S.StyledDateCheckOutWrapper>
       </S.StyledDateInfoWrapper>
-      { booking.booking.status == "validated"
-        ? <S.StyledButtonAbort onClick={ handleCancleButtonMouseClick }>Hủy chuyến đi</S.StyledButtonAbort>
-        : <S.StyledButtonReBook onClick={ handleReBooking }>Đặt lại chuyến đi</S.StyledButtonReBook>
-      }
+      {booking.booking.status == "validated" ? (
+        <S.StyledButtonAbort onClick={handleCancleButtonMouseClick}>
+          Hủy chuyến đi
+        </S.StyledButtonAbort>
+      ) : (
+        <S.StyledButtonReBook onClick={handleReBooking}>
+          Đặt lại chuyến đi
+        </S.StyledButtonReBook>
+      )}
 
-      {isShowCancelPolicyModal && selectedBookingId == booking.booking.booking_id && <CancelPolicyModal onCancel={ handleCancleBooking } booking={ booking }/>}
-      {isShowTripDetailModal && selectedBookingId == booking.booking.booking_id && <TripDetailModal booking={ booking }/>}
+      {isShowCancelPolicyModal &&
+        selectedBookingId == booking.booking.booking_id && (
+          <CancelPolicyModal onCancel={handleCancleBooking} booking={booking} />
+        )}
+      {isShowTripDetailModal &&
+        selectedBookingId == booking.booking.booking_id && (
+          <TripDetailModal booking={booking} />
+        )}
     </S.StyledContainer>
-  )
-}
+  );
+};
--- frontendv2/src/components/List/HomestayList.tsx
+++ frontendv2/src/components/List/HomestayList.tsx
@@ -1,48 +1,57 @@
-import React, { useCallback, useLayoutEffect, useMemo, useState } from 'react';
-import * as S from '@style/comp/List/HomestayList.styled';
-import { HomestayCard } from '../Card/HomestayCard';
-import { useHomestayActions } from '@store/homestay/homestay.actions';
-import { IHomestayGetAllRequestParams, IHomestayGetAllResponse, IHomestaySchema } from '@store/homestay/homestay.schema';
-import { useRecoilValue } from 'recoil';
+import React, { useCallback, useLayoutEffect, useMemo, useState } from "react";
+import * as S from "@style/comp/List/HomestayList.styled";
+import { HomestayCard } from "../Card/HomestayCard";
+import { useHomestayActions } from "@store/homestay/homestay.actions";
+import {
+  IHomestayGetAllRequestParams,
+  IHomestayGetAllResponse,
+  IHomestaySchema,
+} from "@store/homestay/homestay.schema";
+import { useRecoilValue } from "recoil";
 
 interface Props {}
 
 export const HomestayList = (props: Props) => {
   const homestayActions = useHomestayActions();
-  const [homestaysData, setHomestaysData] = useState<IHomestayGetAllResponse | null>(null);
+  const [homestaysData, setHomestaysData] =
+    useState<IHomestayGetAllResponse | null>(null);
 
-  const homestayGetAllRequestParams = useMemo<IHomestayGetAllRequestParams> (() => ({
-    page_id: 1,
-    page_size: 5
-  }), [])
+  const homestayGetAllRequestParams = useMemo<IHomestayGetAllRequestParams>(
+    () => ({
+      page_id: 1,
+      page_size: 10,
+    }),
+    []
+  );
 
   useLayoutEffect(() => {
-    homestayActions.getAll(homestayGetAllRequestParams)
+    homestayActions
+      .getAll(homestayGetAllRequestParams)
       .then((response: IHomestayGetAllResponse) => {
         setHomestaysData(response);
-      })
-  }, [])
+      });
+  }, []);
 
   const createHomestayList = useCallback((data: IHomestaySchema[]) => {
     return data.map((homestay) => (
-      <HomestayCard
-        key={ homestay.id }
-        homestay={ homestay }/>
-    ))
-  }, [])
+      <HomestayCard key={homestay.id} homestay={homestay} />
+    ));
+  }, []);
 
   const renderedHomestayList = useMemo<JSX.Element[] | null>(() => {
     if (homestaysData) {
-      return createHomestayList(homestaysData.homestays.map((item) => item.homestay))
+      return createHomestayList(
+        homestaysData.homestays.map((item) => item.homestay)
+      );
     }
-    return null
-  }, [homestaysData])
+    return null;
+  }, [homestaysData]);
 
   return (
     <S.StyledContainer>
       <S.StyledHomestayListWrapper>
-        { renderedHomestayList }
+        {renderedHomestayList}
       </S.StyledHomestayListWrapper>
     </S.StyledContainer>
-  )
-}
+  );
+};
